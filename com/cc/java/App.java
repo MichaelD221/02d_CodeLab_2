8dd261d6be8df7fd173b878b8febedd50df0ea16
Michael <atlasbook@web.de>
2025-11-25 22:47:12 +0100

    challenge beendet

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,16 +2,34 @@ package com.cc.java;
 
 public class App {
 
+	// Absatz und Trennlinie
+	public static String line = "\n" + "----------------------------------";
+
 	public static void main(String[] args) {
 	
-	
+		Mitarbeiter mitarbeiter1 = new Mitarbeiter("White", "John", "CEO", 2019);
+		output("surname: " + mitarbeiter1.getInfo("familyName"));
+		output("name: " + mitarbeiter1.getInfo("firstName") );
+		output("role: " + mitarbeiter1.getInfo("role") );
+		output("year of entry: " + mitarbeiter1.getInfo("yearOfEntry") + line);
+		
 
+		Mitarbeiter mitarbeiter2 = new Mitarbeiter("Jefferson", "Paula", "Chief Financial Officer", 2022);
+		output("surname: " + mitarbeiter2.getInfo("familyName"));
+		output("name: " + mitarbeiter2.getInfo("firstName") );
+		output("role: " + mitarbeiter2.getInfo("role") );
+		output("year of entry: " + mitarbeiter2.getInfo("yearOfEntry") + line);
+
+
+		Mitarbeiter mitarbeiter3 = new Mitarbeiter("Clean", "Gordy", "Janitor", 2021);
+		output("surname: " + mitarbeiter3.getInfo("familyName"));
+		output("name: " + mitarbeiter3.getInfo("firstName") );
+		output("role: " + mitarbeiter3.getInfo("role") );
+		output("year of entry: " + mitarbeiter3.getInfo("yearOfEntry") + line);
 	}
 
-	
 
 	private static void output(String outStr) {
 		System.out.println(outStr);
 	}
-
 };
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,42 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+    
+    private String familyName;
+    private String firstName;
+    private String role;
+    private int yearOfEntry;
+
+
+    public Mitarbeiter(String familyName, String firstName, String role, int yearOfEntry) {
+        this.familyName = familyName;
+        this.firstName = firstName;
+        this.role = role;
+        this.yearOfEntry = yearOfEntry;
+    }
+    
+
+    public String getInfo(String profil) {
+        // == vergleicht Referenzen - ob zwei Variablen auf dasselbe Objekt zeigen
+        // .equals() vergleicht Inhalte 
+        if (profil.equals("familyName")) {
+            return familyName;
+        }
+        else if (profil.equals("firstName")) {
+            return firstName;
+        } 
+        else if (profil.equals("role")) {
+            return role;
+        }
+        else if (profil.equals("yearOfEntry")) {
+            return Integer.toString(yearOfEntry);
+        }         
+        else {
+            return "Something went wrong";
+        }
+    }
+
+    
+}
+
+
